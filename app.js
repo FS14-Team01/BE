689b37027d117542b3b7d85266db38cfed9787eb
youngsecondbrain-ai <youngsecondbrain-ai   >
2026-09-04 11:35:25 +0900

    feat: 인증 기반 환경 구성

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-import cors from 'cors';
-import 'dotenv/config';
-import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
 
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // route
-app.listen(process.env.PORT ?? 3001, () => console.log('Server Started'));
+app.listen(process.env.PORT ?? 3001, () => console.log("Server Started"));
