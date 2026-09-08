4acae63878b151ab6b79055643ce9577e3744671
yein0528 <o3o_sumyeong@naver.com>
2026-09-08 17:36:05 +0900

    fix: 판매글 ID BIGINT 범위 검증 추가

--- services/sales-service.js
+++ services/sales-service.js
@@ -3,13 +3,20 @@ import { ERROR_DEFINITIONS } from '../errors/error-definitions.js';
 import { findSaleDetailById } from '../repositories/sales-repository.js';
 
 const POSITIVE_INTEGER_PATTERN = /^[1-9]\d*$/;
+const MAX_DATABASE_BIGINT = 9_223_372_036_854_775_807n;
 
 export async function getSaleDetailById(saleId) {
   if (!POSITIVE_INTEGER_PATTERN.test(saleId)) {
     throw new AppError(ERROR_DEFINITIONS.INVALID_REQUEST);
   }
 
-  const sale = await findSaleDetailById(BigInt(saleId));
+  const parsedSaleId = BigInt(saleId);
+
+  if (parsedSaleId > MAX_DATABASE_BIGINT) {
+    throw new AppError(ERROR_DEFINITIONS.INVALID_REQUEST);
+  }
+
+  const sale = await findSaleDetailById(parsedSaleId);
 
   if (!sale) {
     throw new AppError(ERROR_DEFINITIONS.SALE_NOT_FOUND);
