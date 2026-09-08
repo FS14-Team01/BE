ba1b9c14d02f8c44a4d9d65c9171ff0b8510e508
CHOM <apple@appleui-MacBookAir-5.local>
2026-09-08 19:14:38 +0900

    refactor: 판매자 교환 제안 라우트 통합

--- routes/exchange-routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getExchangeOffers } from '../controllers/exchange-controller.js'
-
-const router = express.Router()
-
-router.get('/:saleId/exchange-offers', getExchangeOffers)
-
-export default router
