399674763decad487aba5099198e156532a9e0fe
CHOM <apple@appleui-MacBookAir-5.local>
2026-09-09 10:16:24 +0900

    chore: 파일 포맷팅 적용

--- controllers/exchange-controller.js
+++ controllers/exchange-controller.js
@@ -1,19 +1,19 @@
-import { getExchangeOffersBySale } from '../services/exchange-service.js'
+import { getExchangeOffersBySale } from "../services/exchange-service.js";
 
 async function getExchangeOffers(req, res, next) {
   try {
-    const { saleId } = req.params
-    const userId = req.user?.id
+    const { saleId } = req.params;
+    const userId = req.user?.id;
 
     const result = await getExchangeOffersBySale({
       saleId,
       userId,
-    })
+    });
 
-    return res.status(200).json(result)
+    return res.status(200).json(result);
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-export { getExchangeOffers }
+export { getExchangeOffers };
