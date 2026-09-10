b7e4132f6ea81a0b0ff373831eb28bb0516a5722
youngsecondbrain-ai <youngsecondbrain-ai   >
2026-09-10 11:29:19 +0900

    feat: 본인 정보 확인 기능 구현

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import express from "express";
 import errorHandler from "./middlewares/error-handler.js";
 import authRouter from "./routes/auth-router.js";
 import salesRouter from "./routes/sales-routes.js";
+import userRouter from "./routes/user-router.js";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 // route
 app.use("/auth", authRouter);
 app.use("/sales", salesRouter);
+app.use("/users", userRouter);
 
 // error middleware
 app.use(errorHandler);
